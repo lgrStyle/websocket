1384ebf057aa9a0a3d90540b45e7337fb81eacdc
liguirong <liguirong@toptolink.com>
2020-09-29 14:37:07 +0800

    增加跨域配置

--- src/main/java/com/example/websocket/demo/controller/WebsocketController.java
+++ src/main/java/com/example/websocket/demo/controller/WebsocketController.java
@@ -2,11 +2,13 @@ package com.example.websocket.demo.controller;
 
 import com.example.websocket.demo.OriginalWebsocket;
 import com.example.websocket.demo.common.Response;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WebsocketController {
 
     @RequestMapping("/sendMessage")
